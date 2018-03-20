a86ab04f44752dcbfed1c8db942d3ea6e863683b
Alexey Masolov <amasolov@redhat.com>
2018-03-20 15:44:57 +1100

    final regexp

--- lib/cinch/plugins/karma.rb
+++ lib/cinch/plugins/karma.rb
@@ -29,12 +29,12 @@ module Cinch
         channel = m.channel.name
 
         # Scan messages for multiple karma items
-        m.message.scan(/(\s|\A)(\w+|\(.+?\))(\+\+|--)(\s|\z|$)/).each do |k|
+        m.message.scan(/(^|\s|\A|\b)(\w+|\(.+?\))(\+\+|--)(\s|\z|$)/).each do |k|
 	  m.reply "#{k}"
           process_karma(channel, k[1].gsub(/\(|\)/, '').downcase, k[2])
-#	  item = k[1].gsub(/\(|\)/, '')
-#	  @storage.synced_save(@bot)
-#	  m.reply "#{item} now has #{@storage.data[channel][item]} points of karma"
+	  item = k[1].gsub(/\(|\)/, '')
+	  @storage.synced_save(@bot)
+	  m.reply "#{item} now has #{@storage.data[channel][item]} points of karma"
         end
         @storage.synced_save(@bot)
       end
