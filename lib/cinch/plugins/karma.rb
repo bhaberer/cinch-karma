d720ffcbdd79acb60666b3d4ecede407467f1564
Alexey Masolov <amasolov@redhat.com>
2018-03-20 15:17:11 +1100

    Test

--- lib/cinch/plugins/karma.rb
+++ lib/cinch/plugins/karma.rb
@@ -30,10 +30,11 @@ module Cinch
 
         # Scan messages for multiple karma items
         m.message.scan(/(\s|\A)(\w+|\(.+?\))(\+\+|--)(\s|\z)/).each do |k|
+	  m.reply "#{k}"
           process_karma(channel, k[1].gsub(/\(|\)/, '').downcase, k[2])
-	  item = k[1].gsub(/\(|\)/, '')
-	  @storage.synced_save(@bot)
-	  m.reply "#{item} now has #{@storage.data[channel][item]} points of karma"
+#	  item = k[1].gsub(/\(|\)/, '')
+#	  @storage.synced_save(@bot)
+#	  m.reply "#{item} now has #{@storage.data[channel][item]} points of karma"
         end
         @storage.synced_save(@bot)
       end
